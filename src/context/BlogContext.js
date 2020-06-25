391e112aed1c8ff4fb817905dd5b6e73cba0a047
Tom Kota <63372913+tom2kota@users.noreply.github.com>
2020-06-25 14:31:17 +0300

    Update files

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import React, {useReducer} from 'react';
 
 const BlogContext = React.createContext(undefined, undefined);
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
-  const addBlogPost = () =>
-    setBlogPosts([
-      ...blogPosts,
-      { title: `Blog Post #${blogPosts.length + 1}` },
-    ]);
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost }}>
-      {children}
-    </BlogContext.Provider>
-  );
-};
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost': {
+            return [...state, {title: `Post #${state.length + 1}`}]
+        }
+        default:
+            return state
+    }
+}
 
-export default BlogContext;
+export const BlogProvider = ({children}) => {
+
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+    const addBlogPost = () => dispatch({type: 'add_blogpost'})
+
+    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>{children}</BlogContext.Provider>
+}
+
+export default BlogContext
